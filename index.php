272a3f39c67977f4be50e7c598360e8f1f62ce0c
Erik Correia <81934268+ErikCorreia@users.noreply.github.com>
2023-01-07 22:16:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ use Intervention\Image\ImageManagerStatic as Image;
 
 require 'vendor/autoload.php';
 
+require 'image.php';
+
 $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
@@ -14,11 +16,29 @@ $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
 // Define app routes
-$app->get('/image', function (Request $request, Response $response, $args) {
-  Image::configure(['driver' => 'imagick']);  
-  $image = Image::make('src/foo.jpg')->response('jpg');
-    $response->getBody()->write($image);
-    return $response;
+$app->get('/', function (Request $request, Response $response, $args) {
+
+  Image::configure(['driver' => 'imagick']);
+
+  $wathermark = Image::canvas(300, 50, 'rgba(255,255,255,.1)');
+  $wathermark->text('My Wathermark', 150, 10, function($font) {
+    $font->file('RubikBubbles-Regular.ttf');
+    $font->size(24);
+    $font->color('rgba(255,255,255, .3)');
+    $font->align('center');
+    $font->valign('top');
+    $font->angle(0);
+  });
+  
+  
+  $image = Image::make('src/foo.jpg')
+    ->resize(800, 400)
+    ->insert($wathermark, 'bottom-right', 10, 10)
+    ->fit(800, 400)->response('jpg');
+
+  $response->getBody()->write($image);
+  return $response;
+
 });
 
 // Run app
