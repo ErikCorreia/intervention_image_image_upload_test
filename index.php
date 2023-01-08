46fcad368711ab00f45d9871d059271694cc1d58
Erik Correia <erickcorreia727@gmail.com>
2023-01-07 20:16:50 -0400

    intervention.io image test

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+
+// include composer autoload
+require 'vendor/autoload.php';
+
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManagerStatic as Image;
+
+class ImageManager 
+{    
+
+    public $image;
+    public $size;
+    public $path;
+
+    public function __construct(string $image_origin = 'src/foo.jpg', string $path_to_save_image = 'src/')
+    {
+        // configure with favored image driver (gd by default)
+        Image::configure(['driver' => 'imagick']);
+
+        $this->image = Image::make($image_origin);
+
+        // $this->path = 'storage/images/';
+
+        $this->path = $path_to_save_image;
+        
+    }
+
+    public function createWatherMark()
+    {
+
+        $watermark = Image::canvas(150, 100, '#25a11a');
+        
+        $points = [
+            0,    50,  // Point 1 (x, y)
+            75,   0,   // Point 1 (x, y)
+            150,  50,  // Point 1 (x, y)
+            75,   100, // Point 1 (x, y)
+        ];
+        
+        $watermark->polygon($points, function ($draw) {
+            $draw->background('#eeff01');
+        });
+        
+        $watermark->ellipse(70, 70, 75, 50, function ($draw) {
+            $draw->background('#0035e4');
+        });
+        
+        $watermark->blur(1);
+        $this->image->insert($watermark, 'bottom-right', 100, 100);
+        
+    }
+
+    public function saveImage(string $size = 'thumb'){
+        switch($size){
+            case 'thumb': 
+                $this->image->resize(450, 220);
+                $this->image->fit(450, 220);
+                $this->image->save($this->path . $size.'.jpg');
+            break;
+    
+            case 'medium': 
+                $this->image->resize(800, 520);
+                $this->image->fit(800, 520);
+                $this->image->save($this->path . $size.'.jpg');
+            break;
+                
+            case 'large': 
+                $this->image->resize(1920, 1080);
+                $this->image->fit(1920, 1080);
+                $this->image->save($this->path . $size.'.jpg');
+            break;
+        }
+
+    }
+    public function destroy()
+    {
+        die('5dawsd');
+        // destroy resource
+        $this->image->destroy();
+    }
+}
+
+$img = new ImageManager();
+$img->createWatherMark();
+$img->saveImage('large');
+$img->saveImage('thumb');
+$img->saveImage('medium');
+$img->destroy();
